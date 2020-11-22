9d4cdd6022c0ac0634a2c26a816703ea8d99ba36
Roflersssd <52077572+Roflersssd@users.noreply.github.com>
2020-11-22 19:58:49 +0300

    Laba13

---
+++ Laba13.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include<cstdlib>
+#include <math.h>
+using namespace std;
+
+const double Q = 0.001;//Погрешность
+const double a =0.5;//Начало отрезка
+const double b = 0.6;//Конец отрезка
+
+double f(double x);
+//Функция поиска корня:
+double FindRoot(double(*f)(double), double a, double b, double Q);
+
+int main() {
+    //Интервал, погрешность и корень:
+    double x;
+    cout << "interval:["<<a<<':'<<b<<"]\n";
+    //Проверка корректности интервала:
+    cout << "epsilon: "<<Q<<endl;
+    //Поиск решения:
+    x = FindRoot(f,a, b, Q);
+    cout << "x = " << x << endl;
+    return 0;
+}
+
+double f(double x) {
+    return x * x - sin(5 * x);
+}
+//Функция поиска корня:
+double FindRoot(double(*f)(double), double a, double b, double Q) {
+    double x;
+    if (a == b) {
+        cout << "Error. Root can`t be found\n";
+        return -1;
+    }
+    else if (b - a < Q) {
+        return a;
+    }
+    else {
+        do {
+            x = (a * f(b) - b * f(a)) / (f(b) - f(a));
+            if (f((a + x) / 2) > 0) {
+                b = x;
+            }
+            else a = x;
+
+        } while (fabs(f(x)) > Q);
+    }
+    return x;
+}
---
+++ Laba13_part2.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <math.h>
+#include <locale.h>
+
+const double pi = 4*atan(1);//Число пи
+
+
+double F(double x);//Заданная функция
+
+double Integral(double(*f)(double), double a, double b, double eps);//Функция поиска интеграла
+
+double CalculateMamber(double(*f)(double), double a, double b, int n);//Функция вычисления очередного приближения
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    double a = pi / 2;
+    double b = pi;
+    const double eps = 0.005;
+    printf("Интервал [%.2f;%.2f]\n", a, b);
+    printf("Погрешность eps=%.3f\n", eps);
+    double res = Integral(F, a, b,eps);
+    printf("Интеграл f(x)=cos(x)/x на отрезке [%.2f,%.2f]= %f\n", a, b, res);
+    
+}//main
+
+double F(double x) {
+    return cos(x) / x;
+}//F
+
+
+double Integral(double(*f)(double), double a, double b, double eps) {
+    int n = 10;//количество на которое делится отрезок 
+    int k = 5;//Увеличение разбиения отрезка за одну итерацию
+    double h;//шаг
+    double s0, s1;
+    do {
+        s0 = CalculateMamber(f, a, b, n);
+        n += k;
+        s1 = CalculateMamber(f, a, b, n);
+
+    } while (abs(s0 - s1) > eps);
+    return s1;
+}//Integral
+
+double CalculateMamber(double(*f)(double), double a, double b, int n) {
+    double s = 0;
+    double h = (b - a) / n;
+    for (int i = 0; i < n; i++) {
+        s += f(a + h * i) * h;
+    }//for
+    s += ((f(a) + f(b)) / 2) * h;
+    return s;
+}//CalculateMamber
